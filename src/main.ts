60ce3ef7e32959f40d2d9ca0a489d56ad93c97ef
rosario <58920070+athena272@users.noreply.github.com>
2026-02-03 12:56:40 -0300

    feat(config): load config at bootstrap with APP_INITIALIZER and provideHttpClient

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { ConfigService } from './app/core/services/config.service';
+import { APP_INITIALIZER } from '@angular/core';
+
+function loadConfig(config: ConfigService) {
+  return () => config.loadConfig();
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient()
+    provideHttpClient(),
+    ConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: loadConfig,
+      deps: [ConfigService],
+      multi: true
+    }
   ]
 }).catch(err => console.error(err));
